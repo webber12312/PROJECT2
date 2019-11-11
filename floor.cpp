22645c7f08df14645cc64d3a78e3131eab58e4f0
webber12312 <56022827+webber12312@users.noreply.github.com>
2019-11-11 22:21:00 +0800

    Update floor.cpp

--- floor.cpp
+++ floor.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include<cmath>
 using namespace std;
 
 int battery_row,battery_col;
@@ -17,11 +18,11 @@ typedef struct _Node
 
 class Place
 {
-    public:
+public:
     int row;
     int col;
     Place* next;
-    Place(int x,int y):row(x),col(y),next(NULL){}
+    Place(int x,int y):row(x),col(y),next(NULL) {}
 };
 
 class my_queue
@@ -55,8 +56,10 @@ public:
     }
     int isempty()
     {
-        if(my_front==NULL||my_back==NULL)return true;
-        else return false;
+        if(my_front==NULL||my_back==NULL)
+            return true;
+        else
+            return false;
     }
     int front_row()
     {
@@ -137,6 +140,19 @@ void printBFSpath(int finish_row,int finish_col,Node** node,char**check)
     cur_used_battery++;
 }
 
+void printBFSpath2(int finish_row,int finish_col,Node** node,char**check)
+{
+    if(node[finish_row][finish_col].parent[0]!=-1)
+    {
+        printBFSpath2(node[finish_row][finish_col].parent[0],node[finish_row][finish_col].parent[1],node,check);
+    }
+    if(node[finish_row][finish_col].parent[0]!=-1)
+    {cout<<finish_row<<" "<<finish_col<<endl;
+    check[finish_row][finish_col]='1';
+    step++;
+    cur_used_battery++;}
+}
+
 void printBFSbackpath(int finish_row,int finish_col,Node** node,char**check)
 {
     while(node[finish_row][finish_col].parent[0]!=-1)
@@ -145,7 +161,8 @@ void printBFSbackpath(int finish_row,int finish_col,Node** node,char**check)
         int last_row,last_col;
         last_row=node[finish_row][finish_col].parent[0];
         last_col=node[finish_row][finish_col].parent[1];
-        if(node[last_row][last_col].parent[0]!=-1)cout<<last_row<<" "<<last_col<<endl;
+        if(node[last_row][last_col].parent[0]!=-1)
+            cout<<last_row<<" "<<last_col<<endl;
         finish_row=last_row;
         finish_col=last_col;
         step++;
@@ -166,7 +183,7 @@ bool checkisover(char**check,int map_row,int map_col)
     return true;
 }
 
-void findthenearest(char**check,int*nearest_row,int*nearest_col,int map_row,int map_col,Node**node)
+int findthenearest(char**check,int*nearest_row,int*nearest_col,int map_row,int map_col,Node**node)
 {
     int cur_dis=10000;
     for(int i=0; i<map_row; i++)
@@ -184,10 +201,113 @@ void findthenearest(char**check,int*nearest_row,int*nearest_col,int map_row,int 
             }
         }
     }
+    return cur_dis;
 }
+
+int findthenearest_of_twopoints(char**check,int*nearest_row,int*nearest_col,int map_row,int map_col,int cur_row,int cur_col)
+{
+    int cur_dis=10000;
+    for(int i=0; i<map_row; i++)
+    {
+        for(int j=0; j<map_col; j++)
+        {
+            if(check[i][j]=='0')
+            {
+                if(abs(i-cur_row)+abs(j-cur_col)<cur_dis)
+                {
+                    cur_dis=abs(i-cur_row)+abs(j-cur_col);
+                    *nearest_row=i;
+                    *nearest_col=j;
+                }
+            }
+        }
+    }
+    return cur_dis;
+}
+
+void BFStonearest(char** floor,char**check,int map_row,int map_col,Node** node,int cur_row,int cur_col)
+{
+    my_queue q;
+    q.push(cur_row,cur_col);
+    node[cur_row][cur_col].parent[0]=-1;
+    node[cur_row][cur_col].parent[1]=-1;
+    node[cur_row][cur_col].visited=true;
+    node[cur_row][cur_col].dis=0;
+    while(!q.isempty())
+    {
+        if(q.front_row()-1>=0&&floor[q.front_row()-1][q.front_col()]!='1'&&node[q.front_row()-1][q.front_col()].visited==false)
+        {
+            q.push(q.front_row()-1,q.front_col());
+            //q.setvisit();
+            //q.setparent(q.front_row(),q.front_col());
+            node[q.front_row()-1][q.front_col()].visited=true;
+            node[q.front_row()-1][q.front_col()].parent[0]=q.front_row();
+            node[q.front_row()-1][q.front_col()].parent[1]=q.front_col();
+            node[q.front_row()-1][q.front_col()].dis=node[q.front_row()][q.front_col()].dis+1;
+        }
+        if(q.front_row()+1<map_row&&floor[q.front_row()+1][q.front_col()]!='1'&&node[q.front_row()+1][q.front_col()].visited==false)
+        {
+            q.push(q.front_row()+1,q.front_col());
+            //q.setvisit();
+            //q.setparent(q.front_row(),q.front_col());
+            node[q.front_row()+1][q.front_col()].visited=true;
+            node[q.front_row()+1][q.front_col()].parent[0]=q.front_row();
+            node[q.front_row()+1][q.front_col()].parent[1]=q.front_col();
+            node[q.front_row()+1][q.front_col()].dis=node[q.front_row()][q.front_col()].dis+1;
+        }
+        if(q.front_col()-1>=0&&floor[q.front_row()][q.front_col()-1]!='1'&&node[q.front_row()][q.front_col()-1].visited==false)
+        {
+            q.push(q.front_row(),q.front_col()-1);
+            //q.setvisit();
+            //q.setparent(q.front_row(),q.front_col());
+            node[q.front_row()][q.front_col()-1].visited=true;
+            node[q.front_row()][q.front_col()-1].parent[0]=q.front_row();
+            node[q.front_row()][q.front_col()-1].parent[1]=q.front_col();
+            node[q.front_row()][q.front_col()-1].dis=node[q.front_row()][q.front_col()].dis+1;
+        }
+        if(q.front_col()+1<map_col&&floor[q.front_row()][q.front_col()+1]!='1'&&node[q.front_row()][q.front_col()+1].visited==false)
+        {
+            q.push(q.front_row(),q.front_col()+1);
+            //q.setvisit();
+            //q.setparent(q.front_row(),q.front_col());
+            node[q.front_row()][q.front_col()+1].visited=true;
+            node[q.front_row()][q.front_col()+1].parent[0]=q.front_row();
+            node[q.front_row()][q.front_col()+1].parent[1]=q.front_col();
+            node[q.front_row()][q.front_col()+1].dis=node[q.front_row()][q.front_col()].dis+1;
+        }
+        q.pop();
+    }
+    for(int i=0;i<map_row;i++)
+    {
+        for(int j=0;j<map_col;j++)
+        {
+            node[i][j].visited=false;
+        }
+    }
+}
+
+
+/*void printpath_between_pts(int start_row,int start_col,Node** node,char**check,int finish_row,int finish_col)
+{
+    if(node[finish_row][finish_col].parent[0]!=node[start_row][start_col].parent[0]||node[finish_row][finish_col].parent[1]!=node[start_row][start_col].parent[1])
+    {
+        printpath_between_pts(node[finish_row][finish_col].parent[0],node[finish_row][finish_col].parent[1],node,check,finish_row,finish_col);
+    }
+    cout<<start_row<<" "<<start_col<<endl;
+    check[finish_row][finish_col]='1';
+    step++;
+    cur_used_battery++;
+}*/
+
 
 void DFS(char**check,char**floor,int row,int col,int map_row,int map_col,int* end_row,int* end_col,int battery,Node**node)
 {
+    Node DFS_node[map_row][map_col];
+    Node* DFS_arr_node[map_row];
+    for(int i=0; i<map_row; i++)
+    {
+        DFS_arr_node[i]=DFS_node[i];
+    }
     while(1)
     {
         if(floor[row-1][col]=='0'&&check[row-1][col]=='0'&&row-1>=0)
@@ -252,10 +372,33 @@ void DFS(char**check,char**floor,int row,int col,int map_row,int map_col,int* en
         }
         else
         {
-            check[row][col]='1';
+            /*check[row][col]='1';
             *end_row=row;
             *end_col=col;
-            break;
+            break;*/
+            int nearest_row,nearest_col;
+            BFStonearest(floor,check,map_row,map_col,DFS_arr_node,row,col);
+            int dis=findthenearest(check,&nearest_row,&nearest_col,map_row,map_col,DFS_arr_node);
+            if(checkisover(check,map_row,map_col))
+            {
+                check[row][col]='1';
+                *end_row=row;
+                *end_col=col;
+                break;
+            }
+            if(dis+cur_used_battery+node[nearest_row][nearest_col].dis>battery)
+            {
+                check[row][col]='1';
+                *end_row=row;
+                *end_col=col;
+                break;
+            }
+            else
+            {
+                printBFSpath2(nearest_row,nearest_col,DFS_arr_node,check);
+                row=nearest_row;
+                col=nearest_col;
+            }
         }
     }
 }
@@ -306,9 +449,9 @@ int main()
     cin>>map_row>>map_col>>battery;
     char floor[map_row][map_col];
     char* arr[map_row];
-    for(int i=0;i<map_row;i++)
+    for(int i=0; i<map_row; i++)
     {
-        for(int j=0;j<map_col;j++)
+        for(int j=0; j<map_col; j++)
         {
             char c=getchar();
             if(c=='\n')
@@ -321,7 +464,7 @@ int main()
             }
         }
     }
-    for(int i=0;i<map_row;i++)
+    for(int i=0; i<map_row; i++)
     {
         arr[i]=floor[i];
     }
@@ -332,12 +475,6 @@ int main()
     for(int i=0; i<map_row; i++)
     {
         BFS_arr_node[i]=BFS_node[i];
-    }
-    Node DFS_node[map_row][map_col];
-    Node* DFS_arr_node[map_row];
-    for(int i=0; i<map_row; i++)
-    {
-        DFS_arr_node[i]=DFS_node[i];
     }
 
     char check[map_row][map_col+1];
@@ -370,6 +507,7 @@ int main()
         printBFSbackpath(end_row,end_col,BFS_arr_node,checkarr);
         cur_used_battery=0;
     }
+    cout<<battery_row<<" "<<battery_col<<endl;
     cout<<step<<endl;
     //printBFSpath(1,3,BFS_arr_node);
     //printBFSbackpath(1,3,BFS_arr_node);
